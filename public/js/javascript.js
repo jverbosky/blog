7c2710a61a5c8d36a1d7e005fac98f2a5b76373f
jverbosk <jverbosk@gmail.com>
2017-07-24 23:11:32 -0400

    tweaked function placement and formatting

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -22,19 +22,16 @@ function refreshPage() {
 function postNewBlog() {
 
     $.ajax({
-
-      url: "/ajax_verify",
-      type: "POST",
-      data: JSON.stringify({
-        blog_title: $("#newBlogTitle").val(),
-        blog_date: new Date().toDateString(),
-        blog_text: $("#newBlogText").val()
-      }),
-      dataType: "json",
-      success: function(data, status, xhr) {}
+        url: "/ajax_verify",
+        type: "POST",
+        data: JSON.stringify({
+            blog_title: $("#newBlogTitle").val(),
+            blog_date: new Date().toDateString(),
+            blog_text: $("#newBlogText").val()
+        }),
+        dataType: "json",
+        success: function(data, status, xhr) {}
     });
 
     refreshPage();
 }
-
-$("#btnPostNewBlog").on("click", postNewBlog);
