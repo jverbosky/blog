1adba191909031ad43b49df7a5062f64ba94465e
jverbosk <jverbosk@gmail.com>
2017-08-23 23:23:18 -0400

    troubleshooting heroku error

--- app.rb
+++ app.rb
@@ -117,8 +117,7 @@ get '/prototypes' do
   animals_data = data["Animals"]
 
   # S3 bucket images
-  # images = query_s3(connection)
-  images = []
+  images = query_s3(connection)
 
   erb :prototypes, locals: {animals_data: animals_data, feedback: feedback, animals: animals, habitats: habitats, menus: menus, options: options, images: images}
 
@@ -172,8 +171,7 @@ post '/prototypes' do
 
   # S3 bucket images
   # TODO - sessions weren't working, so calling again here
-  # images = query_s3(connection)
-  images = []
+  images = query_s3(connection)
 
   erb :prototypes, locals: {animals_data: animals_data, feedback: feedback, animals: animals, habitats: habitats, menus: menus, options: options, images: images}
 
