7ac17833c6dda0ba10cbc1337c34e1684b6b627d
jverbosky <jverbosk@gmail.com>
2017-11-09 09:20:13 -0500

    updated base64 string to image prototype to more effectively test base64 string before attempting to render on page

--- public/js/imageConverter.js
+++ public/js/imageConverter.js
@@ -1,19 +1,22 @@
-var b64awsS3images = $("#b64_s3_images");
-var b64uploadImage = $("#b64_upload_image");
-var b64photoPreview = $("#b64_photo_preview");
-var b64imageUrl = $("#b64_image_url");
-var b64invalidImageUrl = $("#b64_invalid_image_url");
-var b64imageUrlInput = $("#b64_img_url_input");
-var b64textArea = $("#b64_string");
-var b64progress = $("#b64_progress");
-var b64copyStringBtn = $("#btn_copy_b64_string");
-var imageTarget = $("#b64_photo_dest");
-var b64string = $("#b64_string_src");
-var btnConvert = $("#convert_b64_string");
-var btnReset = $("#reset_b64_string");
-var msgEmpty = $("#b64_empty_b64_string");
-var msgInvalid = $("#b64_invalid_b64_string");
-var exampleString;
+// Page elements for Image-Base64 String and Base64 String-Image prototypes
+var b64elements = {
+  b64awsS3images: $("#b64_s3_images"),
+  b64uploadImage: $("#b64_upload_image"),
+  b64photoPreview: $("#b64_photo_preview"),
+  b64imageUrl: $("#b64_image_url"),
+  b64invalidImageUrl: $("#b64_invalid_image_url"),
+  b64imageUrlInput: $("#b64_img_url_input"),
+  b64textArea: $("#b64_string"),
+  b64progress: $("#b64_progress"),
+  b64copyStringBtn: $("#btn_copy_b64_string"),
+  imageTarget: $("#b64_photo_dest"),
+  b64string: $("#b64_string_src"),
+  btnConvert: $("#convert_b64_string"),
+  btnReset: $("#reset_b64_string"),
+  msgEmpty: $("#b64_empty_b64_string"),
+  msgInvalid: $("#b64_invalid_b64_string"),
+  exampleString: undefined
+}
 
 
 // Resize panel height for base64 prototype
@@ -27,22 +30,22 @@ function adjustPanel(target) {
 // Reset elements when button options are selected
 function resetB64Elements() {
 
-  b64photoPreview.removeClass("border");
-  b64photoPreview.attr("src", "");
-  b64invalidImageUrl.addClass("div_hide");
-  b64imageUrlInput.val("");
-  b64copyStringBtn.addClass("div_hide");
-  b64textArea.text("");
-  b64textArea.attr("placeholder", "Image base64 string...");
+  b64elements.b64photoPreview.removeClass("border");
+  b64elements.b64photoPreview.attr("src", "");
+  b64elements.b64invalidImageUrl.addClass("div_hide");
+  b64elements.b64imageUrlInput.val("");
+  b64elements.b64copyStringBtn.addClass("div_hide");
+  b64elements.b64textArea.text("");
+  b64elements.b64textArea.attr("placeholder", "Image base64 string...");
 }
 
 
 // Show div for AWS S3 images and hide others
 function b64showS3Images() {
 
-  b64uploadImage.addClass("div_hide");
-  b64imageUrl.addClass("div_hide");
-  b64awsS3images.removeClass("div_hide");
+  b64elements.b64uploadImage.addClass("div_hide");
+  b64elements.b64imageUrl.addClass("div_hide");
+  b64elements.b64awsS3images.removeClass("div_hide");
 
   resetB64Elements();
   adjustPanel("acc_base64");
@@ -52,9 +55,9 @@ function b64showS3Images() {
 // Show div for image upload input and hide others
 function b64showUploadImage() {
 
-  b64awsS3images.addClass("div_hide");
-  b64imageUrl.addClass("div_hide");
-  b64uploadImage.removeClass("div_hide");
+  b64elements.b64awsS3images.addClass("div_hide");
+  b64elements.b64imageUrl.addClass("div_hide");
+  b64elements.b64uploadImage.removeClass("div_hide");
 
   resetB64Elements();
   adjustPanel("acc_base64");
@@ -64,9 +67,9 @@ function b64showUploadImage() {
 // Show div for image URL input and hide others
 function b64showImageUrl() {
 
-  b64awsS3images.addClass("div_hide");
-  b64uploadImage.addClass("div_hide");
-  b64imageUrl.removeClass("div_hide");
+  b64elements.b64awsS3images.addClass("div_hide");
+  b64elements.b64uploadImage.addClass("div_hide");
+  b64elements.b64imageUrl.removeClass("div_hide");
 
   resetB64Elements();
   adjustPanel("acc_base64");
@@ -83,11 +86,11 @@ function copyBase64String() {
 // Display base64 string in textarea field and button to copy string
 function updateBase64TextArea(base64String) {
 
-  b64progress.fadeTo(300, 0);
-  b64textArea.text(base64String);
-  b64copyStringBtn.removeClass("div_hide");
+  b64elements.b64progress.fadeTo(300, 0);
+  b64elements.b64textArea.text(base64String);
+  b64elements.b64copyStringBtn.removeClass("div_hide");
   panelResize("acc_base64");
-  b64progress.html("");
+  b64elements.b64progress.html("");
 }
 
 
@@ -120,13 +123,10 @@ function getBase64FromImageUrl(url, cb) {
     var image = new Image();
 
     image.onload = function () {
-        
+
       var canvas = document.createElement("canvas");
       canvas.width =this.width;
       canvas.height =this.height;
-      imgWidth = Math.floor(canvas.width * 0.6);  // resize for adding image to PDF
-      imgHeight = Math.floor(canvas.height * 0.6);
-
       canvas.getContext("2d").drawImage(this, 0, 0)
 
       cb(canvas.toDataURL("image/png"));
@@ -171,8 +171,8 @@ function parseImageUrl(imgUrl) {
 // Display Font Awesome spinner in base64 textarea while processing image
 function b64InProgress() {
 
-  b64progress.html('<i class="fa fa-refresh fa-3x fa-spin" style="color: #000;"></i>');
-  b64progress.fadeTo(300, 1);
+  b64elements.b64progress.html('<i class="fa fa-refresh fa-3x fa-spin" style="color: #000;"></i>');
+  b64elements.b64progress.fadeTo(300, 1);
 }
 
 
@@ -196,7 +196,7 @@ function cacheS3Image(imgUrl) {
 
 
 // Verify URL is a valid image URL before downloading
-function testImageUrl(url, callback, timeout) {
+function testImageSrc(imgSrc, callback, timeout) {
 
   timeout = timeout || 5000;
   var timedOut = false, timer;
@@ -206,7 +206,7 @@ function testImageUrl(url, callback, timeout) {
 
     if (!timedOut) {
       clearTimeout(timer);
-      callback(url, "error");
+      callback(imgSrc, "error");
     }
   };
 
@@ -214,16 +214,16 @@ function testImageUrl(url, callback, timeout) {
 
     if (!timedOut) {
       clearTimeout(timer);
-      callback(url, "success");
+      callback(imgSrc, "success");
     }
   };
 
-  img.src = url;
+  img.src = imgSrc;
 
   timer = setTimeout(function() {
 
     timedOut = true;
-    callback(url, "timeout");
+    callback(imgSrc, "timeout");
   }, timeout); 
 }
 
@@ -231,34 +231,38 @@ function testImageUrl(url, callback, timeout) {
 // Advise on problem with image URL
 function handleBadUrl() {
 
-  b64invalidImageUrl.removeClass("div_hide");
+  b64elements.b64invalidImageUrl.removeClass("div_hide");
   updateBase64TextArea("");
-  b64copyStringBtn.addClass("div_hide");
+  b64elements.b64copyStringBtn.addClass("div_hide");
 }
 
 
 // Make AJAX request to Sinatra route to prompt caching of non-S3 image to ./public/swap
 function convertImageUrl() {
 
-  b64invalidImageUrl.addClass("div_hide");
+  b64elements.b64invalidImageUrl.addClass("div_hide");
   b64InProgress();
 
-  var imageInfo = b64imageUrlInput.val();
+  var imageUrl = b64elements.b64imageUrlInput.val();
 
-  if (imageInfo.startsWith("http://") || imageInfo.startsWith("https://")) {
-    testImageUrl(imageInfo, getTestResult);
+  if (imageUrl.startsWith("http://") || imageUrl.startsWith("https://")) {
+
+    testImageSrc(imageUrl, getTestResult);
+
   } else {
+
     handleBadUrl();
+
   }
 
-  function getTestResult(url, result) {
+  function getTestResult(imgSrc, result) {
 
     if (result === "success") {
 
       $.ajax({
         url: "/cache_image",
         type: 'POST',
-        data: { image_info: imageInfo, url_type: "non-S3" },
+        data: { image_info: imageUrl, url_type: "non-S3" },
         success: function(result) {
           
           $("#ajax_result").html(result);
@@ -290,6 +294,7 @@ function evaluateB64AndResize(image) {
 
   // Callback function called in getMimeType() to evaluate mimeType for uploaded file
   function evaluateMimeType(mimeType) {
+
     console.log('mimeType: ' + mimeType);
     if (mimeType === 'unknown') {
 
@@ -312,7 +317,7 @@ function evaluateB64AndResize(image) {
         img = document.createElement('img');  // use resized image (blobUrl) for image preview
         img.src = blobUrl;
         document.getElementById(imageSrc).src = blobUrl;
-        b64photoPreview.addClass("border");
+        b64elements.b64photoPreview.addClass("border");
         
         // resize panel after image loaded (delay)
         stUploadImage = setTimeout( function(){
@@ -338,57 +343,65 @@ function evaluateB64AndResize(image) {
 // Clear loaded base64 string and image
 function resetBase64String() {
 
-  imageTarget.attr("src", "");
-  imageTarget.removeClass("border");
-  btnReset.addClass("div_hide");
-  btnConvert.removeClass("div_hide");
-  b64string.val("");
+  b64elements.imageTarget.attr("src", "");
+  b64elements.imageTarget.removeClass("border");
+  b64elements.btnReset.addClass("div_hide");
+  b64elements.btnConvert.removeClass("div_hide");
+  b64elements.b64string.val("");
 }
 
 
 // Hide any visible empty/invalid base64 string messages
 function clearBase64Messages() {
 
-  msgEmpty.addClass("div_hide");
-  msgInvalid.addClass("div_hide");
+  b64elements.msgEmpty.addClass("div_hide");
+  b64elements.msgInvalid.addClass("div_hide");
 }
 
 
 // Load base64 string into img element to preview image
-function loadBase64Image() {
+function renderBase64Image() {
 
-  var b64stringText = b64string.val();
-  var png = "data:image/png;base64";
-  var jpg = "data:image/jpeg;base64";
-  var gif = "data:image/gif;base64";
+  b64elements.imageTarget.attr("src", b64elements.b64string.val());
+  b64elements.imageTarget.addClass("border");
+  b64elements.imageTarget.removeClass("div_hide");
+  b64elements.btnConvert.addClass("div_hide");
+  b64elements.btnReset.removeClass("div_hide");
+
+  stLoadImage = setTimeout( function(){
+    
+    adjustPanel("acc_base64conv");
+    clearTimeout(stLoadImage);
+  }, 100);
+}
+
+
+// Fire off page updates based on base64 string content
+function evalBase64String() {
+
+  var b64stringText = b64elements.b64string.val();
 
   clearBase64Messages();
 
   if (b64stringText !== "") {
 
-    if (b64stringText.startsWith(png) || b64stringText.startsWith(jpg) || b64stringText.startsWith(gif)) {
+    testImageSrc(b64stringText, getTestResult);
 
-      imageTarget.attr("src", b64string.val());
-      imageTarget.addClass("border");
-      imageTarget.removeClass("div_hide");
-      btnConvert.addClass("div_hide");
-      btnReset.removeClass("div_hide");
+    function getTestResult(imgSrc, result) {
 
-      stLoadImage = setTimeout( function(){
-        
+      if (result === "success") {
+
+        renderBase64Image();
+
+      } else {
+
+        b64elements.msgInvalid.removeClass("div_hide");
         adjustPanel("acc_base64conv");
-        clearTimeout(stLoadImage);
-      }, 100);
-
-    } else {
-
-      msgInvalid.removeClass("div_hide");
-      adjustPanel("acc_base64conv");
-
+      }
     }
   } else {
 
-    msgEmpty.removeClass("div_hide");
+    b64elements.msgEmpty.removeClass("div_hide");
     adjustPanel("acc_base64conv");
   }
 }
@@ -399,19 +412,20 @@ function loadExampleString() {
 
   resetBase64String();
   clearBase64Messages();
-  b64string.val("");
+  b64elements.b64string.val("");
 
-  if (exampleString === undefined) {
+  // retrieve contents if exampleString is empty
+  if (b64elements.exampleString === undefined) {
 
     jQuery.get("misc/example_b64.txt", function(data) {
       
-      exampleString = data;
-      b64string.val(exampleString);
+      b64elements.exampleString = data;
+      b64elements.b64string.val(b64elements.exampleString);
     });
 
   } else {
 
-    b64string.val(exampleString);    
+    b64elements.b64string.val(b64elements.exampleString);    
   }
 
   // same result but shorter - reloads each time
