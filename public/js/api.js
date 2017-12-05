e83e5bc7470b2664f1527f5cb723d6ed7878db8f
jverbosk <jverbosk@gmail.com>
2017-12-05 12:12:52 -0500

    refactored api mashup

--- public/js/api.js
+++ public/js/api.js
@@ -1,9 +1,25 @@
+var mashupEl = {
+    apiTitle: $("#apiTitle"),
+    apiImage: $("#apiImage"),
+    apiImageImg: $("#apiImage img"),
+    apiDescription: $("#apiDescription"),
+    apiArticleUrl: $("#apiArticleUrl"),
+    apiResults: $("#api_results"),
+    apiDivider: $("#apiDivider"),
+    searchTerm: $("#searchTerm")
+};
+
+var mashupUrl = {
+    flickrUrl: "https://api.flickr.com/services/feeds/photos_public.gne",
+    wikiUrl: "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search="
+};
+
 // Flickr API example via AJAX
 function getImage(searchTerm) {
-  
+
     $.ajax({
-        url: 'https://api.flickr.com/services/feeds/photos_public.gne',
-        dataType: 'jsonp',
+        url: mashupUrl.flickrUrl,
+        dataType: "jsonp",
         data: { 
             "tags": searchTerm,
             "format": "json"
@@ -20,9 +36,9 @@ function jsonFlickrFeed(json) {
     var randIndex = Math.floor(Math.random() * 20);
     var randImage = json.items[randIndex].media.m;
 
-    $("#apiImage img").attr("src", randImage);
+    mashupEl.apiImageImg.attr("src", randImage);
 
-    $("#apiImage").imagesLoaded().done( function(instance) {
+    mashupEl.apiImage.imagesLoaded().done( function(instance) {
 
         checkWikiContent();
     });
@@ -32,8 +48,8 @@ function jsonFlickrFeed(json) {
 // Wikipedia API example via fetch:
 function getArticleList(searchTerm) {
 
-    var url = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + searchTerm + "&limit=1";
-    
+    var url =  mashupUrl.wikiUrl + searchTerm + "&limit=1"
+
     fetch(url)
     .then(function(resp) {
 
@@ -41,11 +57,11 @@ function getArticleList(searchTerm) {
     })
     .then(function(data) {
 
-        $("#apiTitle").text(data[1][0]);
-        $("#apiDescription").text(data[2][0]);
-        $("#apiArticleUrl").attr("href", data[3][0]);
-        document.getElementById("apiArticleUrl").classList.remove("link_hide");
-        document.getElementById("apiDivider").classList.remove("div_hide");
+        mashupEl.apiTitle.text(data[1][0]);
+        mashupEl.apiDescription.text(data[2][0]);
+        mashupEl.apiArticleUrl.attr("href", data[3][0]);
+        mashupEl.apiArticleUrl.removeClass("link_hide");
+        mashupEl.apiDivider.removeClass("div_hide");
     })
 };
 
@@ -53,14 +69,13 @@ function getArticleList(searchTerm) {
 // Verify Wikipedia content is loaded before resizing panel
 function checkWikiContent() {
 
-    var wikiDivLength = document.getElementById("apiDescription").childNodes.length; // used to determine if content is loaded
+    if (mashupEl.apiTitle !== "") {
 
-    if (wikiDivLength > 0) {
-
-        $("#api_results").removeClass("div_hide");
+        mashupEl.apiResults.removeClass("div_hide");
         panelResize("acc_api");
+        mashupEl.apiResults.fadeIn();
 
-    } else { // if the button hasn't loaded yet, call checkWikiContent() again
+    } else { // if wiki content hasn't loaded yet
 
         setTimeout(function() {
 
@@ -72,11 +87,13 @@ function checkWikiContent() {
 
 $("#submitSearchTerm").on("click", function() {
 
-    $("#apiTitle").text("");
-    $("#apiDescription").text("");
-    $("#apiArticleUrl").attr("href", "");
+    // hide and reset elements on new search
+    mashupEl.apiResults.fadeOut();
+    mashupEl.apiTitle.text("");
+    mashupEl.apiDescription.text("");
+    mashupEl.apiArticleUrl.attr("href", "");
 
-    searchTerm = $("#searchTerm").val();
+    searchTerm = mashupEl.searchTerm.val();
     getArticleList(searchTerm);
     getImage(searchTerm);
 });
